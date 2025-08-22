0f9788b845fbfd6750d907d1aba0ae29f06b0eec
NikAlien <115424518+NikAlien@users.noreply.github.com>
2025-08-22 13:22:54 +0300

    First Commit

---
+++ src/test/java/project/CollaborativeWhiteBoard/CollaborativeWhiteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package project.CollaborativeWhiteBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeWhiteBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
